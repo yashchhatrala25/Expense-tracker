c4d761fa76c89105fcb59524e3f126fbdc440ed5
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-06-01 14:22:46 +0530

    expense tracker code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Expense-tracker
+# expense_tracker
+
+# Packages
+- express 
+- jsonwebtoken 
+- mongoose 
+- dotenv 
+- cors 
+- bcryptjs 
+- multer 
+- xlsx
+- nodemon
+
+# Generate JWT Token
+- node -e "console.log(require('crypto').randomBytes(64).toString('hex'))"
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    method: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
