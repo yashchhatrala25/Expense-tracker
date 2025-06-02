3cbb1b62d467caa6669126845e30842d4de5fd01
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-06-02 23:23:05 +0530

    sidebar use and get data

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  useUserAuth();
 
-export default Home
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Something went wrong. Please try again.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData() 
+  
+    return () => {}
+  }, [])
+  
+
+  return (
+    <DashboardLayout activeMenu="Dashboared">
+      <div className="my-5 mx-auto">Home</div>
+    </DashboardLayout>
+  );
+};
+
+export default Home;
