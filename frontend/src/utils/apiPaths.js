925e33f6b43e9229201eee95885276dcf8679481
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-06-23 22:33:10 +0530

    refactor code

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,4 @@
-// export const BASE_URL = "https://expense-tracker-73yd.onrender.com";
-export const BASE_URL = "https://expense-tracker-flax-three-13.vercel.app/";
+export const BASE_URL = "https://expense-tracker-73yd.onrender.com";
 
 // utils/apiPaths.js
 export const API_PATHS = {
