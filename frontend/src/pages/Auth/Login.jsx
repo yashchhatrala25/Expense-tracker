8708e39c10270ec719fc8958e67c8e6fe57a8d21
Dev Kotecha <devkotecha2022@gmail.com>
2025-06-23 15:07:03 +0530

    refa: remove unnecessary code

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -11,7 +11,6 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-  console.log("Login Page Rendered");
 
   const { updateUser } = useContext(UserContext);
 
